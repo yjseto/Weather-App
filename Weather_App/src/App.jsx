444c5596227e0d698b30e9686876966d82174333
Youji Seto <setoyouji@gmail.com>
2024-03-09 12:15:25 -0900

    Weather App - react JS

---
+++ Weather_App/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Weather from './weather';
+import './App.css'
+
+const App = () => {  
+  return (
+    <div>
+      <h1>Youji Weather App (My Intro to JSX)</h1>
+      <Weather />
+    </div>
+  )
+};
+
+export default App
